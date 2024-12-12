e6e8c7f4da1ced0da3fb64fc23185ae912219929
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2024-12-12 15:51:11 +0000

    rename tree

---
+++ mistreeplus/tree/__init__.py
@@ -0,0 +1,5 @@
+
+from .groups import get_groups
+
+from .tree import get_adjacents
+from .tree import adjacents2tree
--- mistreeplus/trees/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .groups import get_groups
-
-from .trees import get_adjacents
-from .trees import adjacents2tree
